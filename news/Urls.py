999c8357f04d50aef1853a6c4a9fc8676ae23127
AleksKhokrin <88559646+AleksKhokrin@users.noreply.github.com>
2021-11-12 18:29:25 +0300

    Update Urls.py

--- news/Urls.py
+++ news/Urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
-from .views import PostsList, SearchList, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from .views import NewsList, NewsDetail, Search, NewsCreate, NewsUpdate, NewsDelete, CategoryView
+from .views import subscribe_me, unsubscribe_me
 
 urlpatterns = [
-    
-    path('', PostsList.as_view()),
-    path('search/', SearchList.as_view()),
-    
-    path('<int:pk>/', PostDetailView.as_view(), name='post_detail'),  
-    path('create/', PostCreateView.as_view(), name='post_create'),  
-    path('create/<int:pk>', PostUpdateView.as_view(), name='post_update'),
-    path('delete/<int:pk>', PostDeleteView.as_view(), name='post_delete'),
+    path('', NewsList.as_view(), name='news'),
+    path('<int:pk>', NewsDetail.as_view(), name='news_detail'),
+    path('search/', Search.as_view(), name='news_search'),
+    path('add/', NewsCreate.as_view(), name='news_add'),
+    path('<int:pk>/edit/', NewsUpdate.as_view(), name='news_edit'),
+    path('<int:pk>/delete/', NewsDelete.as_view(), name='news_delete'),
+    path('categories/', CategoryView.as_view(), name='subscribes'),
+    path('categories/<int:cat_id>/subscribes/', subscribe_me, name='subscribes'),
+    path('categories/<int:cat_id>/unsubscribes/', unsubscribe_me, name='subscribes'),
 ]
