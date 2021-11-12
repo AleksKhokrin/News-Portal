15fe2416fa09c8a5bc8bb6bc48fc46fd08d5c345
AleksKhokrin <88559646+AleksKhokrin@users.noreply.github.com>
2021-11-12 18:28:38 +0300

    Create signals.py

---
+++ news/signals.py
@@ -0,0 +1,35 @@
+from django.db.models.signals import post_save
+from decouple import config
+from django.dispatch import receiver  
+from .models import Post
+from django.template.loader import render_to_string
+from django.core.mail import EmailMultiAlternatives
+
+
+@receiver(post_save, sender=Post)
+def notify_subscribers(sender, instance, created, **kwargs):
+    subscribers = instance.post_category.subscribers.all()
+    print(subscribers)
+    address = []
+    for a in subscribers:
+        address.append(a.email)
+    if created:
+        subject = f'Новость: {instance.title}'
+    else:
+        subject = f'Новость изменена: {instance.title}'
+
+    html_content = render_to_string(
+        'news_created.html',
+        {
+            'post': instance,
+        }
+    )
+    msg = EmailMultiAlternatives(
+        subject,
+        body=instance.text,
+        from_email=config('EMAIL_HOST_USER'),
+        to=[*address],
+    )
+    msg.attach_alternative(html_content, "text/html")  # добавляем html
+
+    msg.send()
