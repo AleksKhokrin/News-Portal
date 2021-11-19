ff755c00efeaad18a046004d84199f9890d2156e
AleksKhokrin <88559646+AleksKhokrin@users.noreply.github.com>
2021-11-19 21:00:37 +0300

    Update Views.py

--- news/Views.py
+++ news/Views.py
@@ -2,17 +2,22 @@ from django.shortcuts import render, reverse, redirect
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.cache import cache_page
+from django.core.cache import cache  # импортируем наш кэш
 
 from .forms import PostForm
 from .models import Post, Category
 from .filters import PostFilter
 
 
+# Create your views here.
+
+
 class NewsList(ListView):
     model = Post
     template_name = 'news_list.html'
     context_object_name = 'news'
-    paginate_by = 2
+    paginate_by = 5
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -24,18 +29,29 @@ class NewsDetail(DetailView):
     template_name = 'news_detail.html'
     queryset = Post.objects.all()
 
+    def get_object(self, *args, **kwargs):
+        obj = cache.get(f'post-{self.kwargs["pk"]}', None)
+
+        if not obj:
+            obj = super().get_object()
+            cache.set(f'post-{self.kwargs["pk"]}', obj)
+
+        return obj
+
 
 class Search(ListView):
     model = Post
     template_name = 'search.html'
     context_object_name = 'news'
     ordering = ['-date_creation']
-    paginate_by = 2  
+    paginate_by = 2  # поставим постраничный вывод в 10 элементов
 
-    def get_context_data(self, **kwargs):  
+    def get_context_data(self, **kwargs):  # забираем отфильтрованные объекты переопределяя
+        # метод get_context_data у наследуемого класса (привет полиморфизм, мы скучали!!!)
         context = super().get_context_data(**kwargs)
         context['filter'] = PostFilter(self.request.GET, queryset=self.get_queryset())  # вписываем наш
-               return context
+        # фильтр в контекст
+        return context
 
 
 class NewsCreate(PermissionRequiredMixin, CreateView):
@@ -72,6 +88,7 @@ class CategoryView(ListView):
 
 
 @login_required
+@cache_page(60 * 5)
 def subscribe_me(request, cat_id):
     user = request.user
     category = Category.objects.get(pk=cat_id)
@@ -81,6 +98,7 @@ def subscribe_me(request, cat_id):
 
 
 @login_required
+@cache_page(60 * 5)
 def unsubscribe_me(request, cat_id):
     user = request.user
     category = Category.objects.get(pk=cat_id)
